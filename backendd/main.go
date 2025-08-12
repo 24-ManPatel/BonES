d9996534e96c78c0f47f6a23cf40b0514ffc50af
24-ManPatel <manpatel240406@gmail.com>
2025-08-12 09:50:23 +0530

    Package issue solved 2

--- backendd/main.go
+++ backendd/main.go
@@ -1,34 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "context"
+    "fmt"
+    "log"
+    "time"
 
-	"backendd/config"
-    "backendd/handlers"
-
-
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "github.com/joho/godotenv"
+    "os"
 )
 
 func main() {
-	// Load env
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    // Load .env
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	// Connect DB
-	config.ConnectDB()
+    mongoURI := os.Getenv("MONGO_URI")
+    dbName := os.Getenv("MONGO_DB")
+    collectionName := os.Getenv("MONGO_COLLECTION")
 
-	// Router
-	r := mux.NewRouter()
-	r.HandleFunc("/api/matches", handlers.GetMatches).Methods("GET")
+    // Connect to MongoDB
+    client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	port := os.Getenv("PORT")
-	fmt.Printf("🚀 Server running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Disconnect(ctx)
+
+    // Fetch data
+    collection := client.Database(dbName).Collection(collectionName)
+
+    cursor, err := collection.Find(ctx, bson.M{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer cursor.Close(ctx)
+
+    fmt.Println("=== Matches from MongoDB Atlas ===")
+    for cursor.Next(ctx) {
+        var match bson.M
+        if err := cursor.Decode(&match); err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(match)
+    }
+
+    if err := cursor.Err(); err != nil {
+        log.Fatal(err)
+    }
 }
